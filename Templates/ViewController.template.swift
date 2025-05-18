f7ff0165410f52a996e9c1c4f17982459e6965e1
jonathanduran7 <joniduran624@gmail.com>
2025-05-18 16:04:56 -0300

    modify template for view controller

--- Templates/ViewController.template.swift
+++ Templates/ViewController.template.swift
@@ -1,9 +1,35 @@
+import RxSwift
 import UIKit
 
-class {{ScreenName}}ViewController: UIViewController {
+final class {{ScreenName}}ViewController: BaseViewController {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-        title = "{{ScreenName}}"
+        
+        layout()
+        bind()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+}
+
+private extension {{ScreenName}}ViewController {
+    func layout(){
+        
+    }
+    
+    func bind(){
+        
+    }
+    
+    func bindRx(){
+        
     }
 }
